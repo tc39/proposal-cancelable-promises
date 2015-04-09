59811554eb740627395d5205e4d1018f3e0a83fb
Domenic Denicola <d@domenic.me>
2015-04-09 17:41:53 -0400

    Super-beginning implementation and tests

--- test/cancelable-promise.js
+++ test/cancelable-promise.js
@@ -1,3 +1,31 @@
+import CancelablePromise from "../lib/cancelable-promise";
+const assert = require("assert");
+
 describe("Cancelable Promise Tests", () => {
-  // none yet
+  it("should call the onCanceled handler when canceling a forever-pending promise", () => {
+    let called = false;
+    const p = new CancelablePromise(() => {}, () => called = true);
+
+    p.cancel();
+
+    assert(called, "onCanceled should be called");
+  });
+
+  it("should not call the onCanceled handler when canceling an immediately-fulfilled promise", () => {
+    let called = false;
+    const p = new CancelablePromise(resolve => resolve(), () => called = true);
+
+    p.cancel();
+
+    assert(!called, "onCanceled should not be called");
+  });
+
+  it("should not call the onCanceled handler when canceling an immediately-rejected promise", () => {
+    let called = false;
+    const p = new CancelablePromise((resolve, reject) => reject(), () => called = true);
+
+    p.cancel();
+
+    assert(!called, "onCanceled should not be called");
+  });
 });

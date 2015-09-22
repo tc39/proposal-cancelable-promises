8603dd50db8a7f2995a14990c0f38f5bcd57f8f3
Domenic Denicola <d@domenic.me>
2015-09-22 13:25:48 -0700

    Make cancelation propagation work again
    The "default reaction" to canceled promises should be to propagate their canceled-ness, not to resolve.

--- test/cancel.js
+++ test/cancel.js
@@ -77,47 +77,37 @@ describe("Basic canceled interaction", () => {
       assert(onCanceledCalled, "onCanceled called");
     });
   });
+});
 
-  it.skip("should propogate cancelation", () => {
+describe("Cancelation propagation through non-branching chains", () => {
+  it("should call onCanceled for derived promises", () => {
     let cancel;
     const p = new Promise((resolve, reject, c) => cancel = c);
 
-    let onFinallyCalled = false;
-    let onFulfillCalled = false;
-    let onRejectCalled = false;
-    let secondOnFinallyCalled = false;
+    let onFulfilledCalled = false;
+    let onRejectedCalled = false;
+    let onCanceledCalled = false;
+    let onCanceled2Called = false;
 
-    p.finally(_ => {
-      onFinallyCalled = true;
-    }).then(_ => {
-      onFulfillCalled = true;
-    }, _ => {
-      onRejectCalled = true;
-    }).finally(_ => {
-      secondOnFinallyCalled = true;
-    });
+    p
+      .then(
+        () => onFulfilledCalled = true,
+        () => onRejectedCalled = true,
+        () => onCanceledCalled = true
+      )
+      .then(
+        () => onFulfilledCalled = true,
+        () => onRejectedCalled = true,
+        () => onCanceled2Called = true
+      );
 
     cancel();
 
     return delay().then(_ => {
-      assert(onFinallyCalled, "onFinally should be called");
-      assert(!onFulfillCalled, "onFulfill should not be called");
-      assert(!onRejectCalled, "onReject should not be called");
-      assert(secondOnFinallyCalled, "2nd onFinally should be called");
-    });
-  });
-});
-
-describe.skip("Cancelation propagation through non-branching chains", () => {
-  it("should call chained finallies", done => {
-    const p = new Promise((resolve, reject, cancel) => cancel());
-
-    let callCount = 0;
-
-    p.finally(_ => callCount++).finally(_ => {
-      callCount++;
-      assert.strictEqual(callCount, 2, "Both finallies called");
-      done();
+      assert.strictEqual(onFulfilledCalled, false, "onFulfilled not called");
+      assert.strictEqual(onRejectedCalled, false, "onRejected not called");
+      assert(onCanceledCalled, "onCanceled called");
+      assert(onCanceled2Called, "second onCanceled called");
     });
   });
 });

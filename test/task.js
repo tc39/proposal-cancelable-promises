b92d6834900fe19009fc57f56490d75f1be68ce9
Domenic Denicola <d@domenic.me>
2016-05-18 18:00:40 -0400

    Hack around horrific SweetJS issue

--- test/task.js
+++ test/task.js
@@ -3,6 +3,16 @@ const assert = require("assert");
 const Task = require("../lib/task.js");
 const delay = require("./helpers/delay.js");
 
+describe("Task.prototype.then basics", () => {
+  it("should return a Task instance", () => {
+    const t = new Task(() => {});
+    const descendant = t.then();
+
+    assert.ok(descendant instanceof Task, "descendant should be instanceof Task");
+    assert.strictEqual(descendant.constructor, Task, "descendant.construct should be Task");
+  });
+});
+
 describe("Task.prototype.cancel basics", () => {
   it("should cause the task to be canceled with the given reason (subscribe before)", () => {
     const reason = { some: "reason" };

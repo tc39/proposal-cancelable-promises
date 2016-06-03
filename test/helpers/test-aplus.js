e9e9358bbbb24cf6c62aa2156e19fb241ad94c46
Domenic Denicola <d@domenic.me>
2016-06-03 16:54:04 -0400

    Remove task-related stuff

--- test/helpers/test-aplus.js
+++ test/helpers/test-aplus.js
@@ -1,21 +1,24 @@
 "use strict";
 require("./install-syntax.js");
-
+const Promise = require("../../lib/promise.js");
 const promisesAplusTests = require("promises-aplus-tests");
-const promiseAdapter = require("../aplus-adapters/promise.js");
-const taskAdapter = require("../aplus-adapters/task.js");
 
-promisesAplusTests(promiseAdapter, err => {
+const adapter = {
+  resolved: v => Promise.resolve(v),
+  rejected: r => Promise.reject(r),
+  deferred: () => {
+    const d = {};
+    d.promise = new Promise((resolve, reject) => {
+      d.resolve = resolve;
+      d.reject = reject;
+    });
+    return d;
+  }
+};
+
+promisesAplusTests(adapter, err => {
   if (err) {
     console.log(err);
     process.exit(1);
-    return;
   }
-
-  promisesAplusTests(taskAdapter, err => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-  });
 });
